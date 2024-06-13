11b09c8c5d1b521726f31b89d3d82fd77d1c701d
Saishashank Petkar <saishashankp@infinite.com>
2024-06-13 11:28:31 +0530

    Day 1.1 Update

---
+++ Csharp/Day1_Dotnet_project/Day1_Dotnet_project/myclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Day1_Dotnet_project
+{
+    class myclass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C sharp classes.");
+            Console.WriteLine("Please Enter Your FirstName");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Please Enter Your LastName");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Your First Name is " + fname + " and LastName is "+ lname);//concatenation
+            Console.WriteLine("Your Name is {0} and Last Name is {1}", fname,lname);//placeholder substitution
+            Console.WriteLine($"Your First Name is {fname} and Last Name is {lname}");//string interpolation
+            Console.Read();
+
+        }
+    }
+}

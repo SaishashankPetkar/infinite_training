b15863f15b6c42264c611e2c243dd7b94f79e754
Saishashank Petkar <saishashankp@infinite.com>
2024-06-13 14:19:45 +0530

    Dotnet project update

--- Csharp/Day1_Dotnet_project/Day1_Dotnet_project/myclass.cs
+++ Csharp/Day1_Dotnet_project/Day1_Dotnet_project/myclass.cs
@@ -15,8 +15,24 @@ namespace Day1_Dotnet_project
             Console.WriteLine("Your First Name is " + fname + " and LastName is "+ lname);//concatenation
             Console.WriteLine("Your Name is {0} and Last Name is {1}", fname,lname);//placeholder substitution
             Console.WriteLine($"Your First Name is {fname} and Last Name is {lname}");//string interpolation
+            //to call a non-static function
+            //we need to have an object
+            myclass calss = new myclass();
+            calss.Display("Sai");
+            //to call a static function of class,we dont need an object
+            int sum = Addnos(5, 6);
+            Console.WriteLine("The sum is:" + sum);
             Console.Read();
             /*Program to understand input and output functionalityes in a console based application*/
+        }
+        void Display(string username) //function signature
+        {
+            Console.WriteLine("Welcome " + username); //funtion defination/body
+        }
+
+        static int Addnos(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
